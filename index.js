10323ef8488df26f02b980e83d67b22641238f73
sojibdasjr <sojibdas47@gmail.com>
2021-12-19 03:33:25 +0600

    added

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ async function run (){
             res.send(teachers);
         });
 
+        app.get('/users/:email', async(req, res)=>{
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if(user.role === 'admin'){
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin});
+
+        })
+
         // POST ALL API
 
         // users post api
@@ -66,6 +78,14 @@ async function run (){
             const option = {upsert:true};
             const updateDoc = {$set:user};
             const result = await userCollection.updateOne(filter, updateDoc, option);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async(req, res)=>{
+            const user = req.body;
+            const filter = {email: user.email};
+            const updateDoc = {$set:{role:'admin'}};
+            const result = await userCollection.updateOne(filter, updateDoc);
             res.json(result);
         })
 
